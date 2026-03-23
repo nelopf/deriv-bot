7852943d77d82929f41983db0c81a9e64c49aa0d
nelopf <nelsonpaulo444@gmail.com>
2026-03-23 08:08:53 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# =============================================
-# CONFIGURAÇÕES DO BOT
-# =============================================
-
-TELEGRAM_TOKEN = "8654852978:AAGijmqYv9WnwJOHNBjFgPiMvjBqNPgE7Dw"
-CHAT_ID = "7635744352"
-DERIV_APP_ID = "1089"
-DERIV_API_TOKEN = ""
-STAKE = 1.00
